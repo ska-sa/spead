8dd8ac57bf37238269362ddb5570ae395fa9fc19
Adam Barta <adam@ska.ac.za>
2013-05-07 00:05:59 +0200

    test

--- gpu/shared.c
+++ gpu/shared.c
@@ -194,7 +194,7 @@ int setup_ocl(char *kf, cl_context *context, cl_command_queue *command_queue, cl
   char name[100];
 
   char *fc;
-  int fd, i;
+  int fd, i, j;
   struct stat fs;
 
   if (kf == NULL || context == NULL || command_queue == NULL || program == NULL){
@@ -280,45 +280,45 @@ int setup_ocl(char *kf, cl_context *context, cl_command_queue *command_queue, cl
 #ifdef DEBUG
     fprintf(stderr, "Device name: %s\n", name);
 #endif
-  }
 
 #if 1
 #ifdef DEBUG
-  
-  cl_bool ecc;
-  clGetDeviceInfo(devices[0], CL_DEVICE_ERROR_CORRECTION_SUPPORT, sizeof(ecc), &ecc, NULL);
-  fprintf(stderr, "ECC: %d\n", ecc);
 
-  cl_uint units;
-  clGetDeviceInfo(devices[0], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(units), &units, NULL); 
-  fprintf(stderr, "Max clock frequency: %d\n", units);
+    cl_bool ecc;
+    clGetDeviceInfo(devices[i], CL_DEVICE_ERROR_CORRECTION_SUPPORT, sizeof(ecc), &ecc, NULL);
+    fprintf(stderr, "ECC: %d\n", ecc);
 
-  clGetDeviceInfo(devices[0], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(units), &units, NULL); 
-  fprintf(stderr, "Max compute units (multiprocessors): %d\n", units);
+    cl_uint units;
+    clGetDeviceInfo(devices[i], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(units), &units, NULL); 
+    fprintf(stderr, "Max clock frequency: %d\n", units);
 
-  cl_ulong localmemsize;
-  clGetDeviceInfo(devices[0], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(localmemsize), &localmemsize, NULL); 
-  fprintf(stderr, "Global memsize: %ld\n", localmemsize);
+    clGetDeviceInfo(devices[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(units), &units, NULL); 
+    fprintf(stderr, "Max compute units (multiprocessors): %d\n", units);
 
-  clGetDeviceInfo(devices[0], CL_DEVICE_LOCAL_MEM_SIZE, sizeof(localmemsize), &localmemsize, NULL); 
-  fprintf(stderr, "Local memsize: %ld\n", localmemsize);
+    cl_ulong localmemsize;
+    clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(localmemsize), &localmemsize, NULL); 
+    fprintf(stderr, "Global memsize: %ld\n", localmemsize);
 
-  size_t wgs;
-  clGetDeviceInfo(devices[0], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(wgs), &wgs, NULL); 
-  fprintf(stderr, "Max work group size: %ld\n", wgs);
+    clGetDeviceInfo(devices[i], CL_DEVICE_LOCAL_MEM_SIZE, sizeof(localmemsize), &localmemsize, NULL); 
+    fprintf(stderr, "Local memsize: %ld\n", localmemsize);
 
-  clGetDeviceInfo(devices[0], CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(units), &units, NULL); 
-  fprintf(stderr, "Max work item dimentions: %d\n", units);
+    size_t wgs;
+    clGetDeviceInfo(devices[i], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(wgs), &wgs, NULL); 
+    fprintf(stderr, "Max work group size: %ld\n", wgs);
 
-  size_t wid[units];
-  clGetDeviceInfo(devices[0], CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(size_t)*units, &wid, NULL); 
-  fprintf(stderr, "Max work item sizes: ");
+    clGetDeviceInfo(devices[i], CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(units), &units, NULL); 
+    fprintf(stderr, "Max work item dimentions: %d\n", units);
 
-  for (i=0; i <units;i++)
-    fprintf(stderr, "%d ", wid[i]);
-  fprintf(stderr, "\n");
+    size_t wid[units];
+    clGetDeviceInfo(devices[i], CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(size_t)*units, &wid, NULL); 
+    fprintf(stderr, "Max work item sizes: ");
+
+    for (j=0; j<units;j++)
+      fprintf(stderr, "%d ", wid[j]);
+    fprintf(stderr, "\n");
 #endif
 #endif
+  }
 
 
 
