d0cba13bf64d53dcd6c8d83d45d51bfb2d35c25d
Simon Ratcliffe <simonr@ska.ac.za>
2014-02-19 14:40:40 +0200

    Rework of bf_decode code to include environment variable configuration
    Added the following environmental variables to allow the defaults
    to be overriden:
    
    PREFIX - change the filepath prefix (default /data2/latest)
    FULLBAND - record entire band instead of inner 50% (default 0)
    TRANPOSE - tranpose time frequency blocks from correlator (default 0)
    
    e.g.
    
     $ PREFIX=/home/kat/data TRANPOSE=1 speadrx -d bf_decode.so
    
    The restriction on a specific beam has also been removed and bf_decode
    will now record any beamformer data arriving on the specified port.

--- gpu/bf_decode.c
+++ gpu/bf_decode.c
@@ -8,7 +8,8 @@
 #include <time.h>
 #include <spead_api.h>
 
-#define SPEAD_BF_DATA_ID    0xb000
+#define SPEAD_BF_DATA_ID_0  0xb000
+#define SPEAD_BF_DATA_ID_1  0xb001
 #define SPEAD_TS_ID 0x1600
 #define TS_PER_HEAP 128
  // number of time samples per heap
@@ -16,6 +17,10 @@
  // number of channels per heap
 #define BYTES_PER_SAMPLE 2
 #define FILE_HEAP_LEN 8000
+
+#define DEFAULT_PREFIX "/data2/latest"
+#define DEFAULT_TRANPOSE 0
+#define DEFAULT_FULLBAND 0
 
 #define EXPECTED_HEAP_LEN TS_PER_HEAP * CHANS_PER_HEAP * BYTES_PER_SAMPLE
 
@@ -38,8 +43,14 @@ struct snap_shot {
   int flag[4];
   int master_pid;
   unsigned long long prior_ts;
-  char filename[100];
+  char filename[255];
+   // defaults below
+  char * prefix;
+  int transpose;
+  int fullband;
 };
+ // added several default values that potentially get overriden 
+ // by environment variables.
 
 void spead_api_destroy(struct spead_api_module_shared *s, void *data)
 {
@@ -69,6 +80,7 @@ void spead_api_destroy(struct spead_api_module_shared *s, void *data)
 void *spead_api_setup(struct spead_api_module_shared *s)
 {
   struct snap_shot *ss;
+  char * env_temp;
 
   ss = NULL;
 
@@ -89,12 +101,25 @@ void *spead_api_setup(struct spead_api_module_shared *s)
     ss->prior_ts = 0;
     ss->master_pid = 0;
 
+     // change defaults based on environmental variables present
+    env_temp = getenv("TRANSPOSE");
+    if (env_temp != NULL) ss->transpose = atoi(env_temp);
+    else ss->transpose = DEFAULT_TRANPOSE;
+
+    env_temp = getenv("FULLBAND");
+    if (env_temp != NULL) ss->fullband = atoi(env_temp);
+    else ss->fullband = DEFAULT_FULLBAND;
+
+    env_temp = getenv("PREFIX");
+    if (env_temp != NULL) ss->prefix = env_temp;
+    else ss->prefix = DEFAULT_PREFIX;
+
     set_data_spead_api_module_shared(s, ss, sizeof(struct snap_shot));
  
-    fprintf(stderr, "%s: PID [%d] decode BF data %s\n", __func__, getpid());
+    fprintf(stderr, "%s: PID [%d] decode BF data\n", __func__, getpid());
+    fprintf(stderr, "\nConfiguration\n=============\nFilename Prefix (PREFIX): %s\nTranspose data (TRANPOSE): %s\nRecord Fullband (FULLBAND): %s\n\n", ss->prefix, ss->transpose ? "True":"False", ss->fullband ? "True":"False");
 
     fflush(stdout);
-  
   }
  
   heap_count = 1; 
@@ -114,9 +139,8 @@ void transpose_bf_block(void *input,void *output, uint64_t data_len, uint64_t st
  int channels = CHANS_PER_HEAP / 2;
   // only include center half of the band
  int rows = stride;
- int chan,ts,tscols,blocksize,offset_in,offset_out,inner_temp;
- int i,j,k,l;
- uint16_t rr;
+ int chan,ts,blocksize,offset_in,offset_out,inner_temp;
+ int k;
  in = input;
  out = output;
 
@@ -125,11 +149,6 @@ void transpose_bf_block(void *input,void *output, uint64_t data_len, uint64_t st
 #endif
  blocksize = 8;
   // keep this fixed !!
-/* for (ts = 0; ts < rows; ts++) {
-  for (chan = 0; chan < cols; chan++) {
-   out[chan + (ts*cols)] = in[ts + (chan*rows)];
-  }
- }*/
 
  #pragma omp parallel for
  for (ts=0; ts < rows; ts+=blocksize) {
@@ -151,23 +170,21 @@ void transpose_bf_block(void *input,void *output, uint64_t data_len, uint64_t st
  }
 } // end of transpose_bf_block
 
-int write_bf_data(void *data, uint64_t data_len, unsigned long offset)
+int write_bf_data(void *data, uint64_t data_len, unsigned long offset, int divisor, int transpose)
 {
- uint8_t re, im, *dp, *da;
- uint64_t tcks;
- int i;
-
- dp = malloc(sizeof(uint8_t) * data_len / 2);
-  // half the output length as we are only keeping the middle half of the band
+ uint8_t *dp, *da;
+ uint64_t data_len_div = data_len / divisor;
 
  da = data ;
 
-#ifdef DEBUG
- fprintf(stderr, "Channel 322, timestamp 14: %u %u %u %u",da[28 + (256*322)],da[29 + (256*322)],dp[644 + (14*2048)],dp[645 + (14*2048)]);
-#endif
+ if (transpose) {
+  dp = malloc(sizeof(uint8_t) * data_len_div);
+  transpose_bf_block(da, dp, data_len_div, TS_PER_HEAP, BYTES_PER_SAMPLE);
+ }
+ else dp = da;
 
  if (w_fd2 == NULL) { fprintf(stderr,"!!!!! No file !!!!"); return -1;}
- if (pwrite(w_fd2, da + data_len/4, data_len/2, ((data_len / 2 ) * offset)) < 0) {
+ if (pwrite(w_fd2, dp + (data_len - data_len_div) / divisor, data_len_div, data_len_div * offset) < 0) {
   perror("Failed to write.");
   fprintf(stderr,"%s: Failed to write data to disk in thread %d\n",__func__,getpid());
  };
@@ -182,26 +199,26 @@ int write_bf_data(void *data, uint64_t data_len, unsigned long offset)
 int spead_api_callback(struct spead_api_module_shared *s, struct spead_item_group *ig, void *data)
 {
   struct snap_shot *ss;
-  int file_flag,i;
-  unsigned long long ts,ts2;
+  unsigned long long ts;
   struct spead_api_item *itm;
-  struct stat sb;
 
-  file_flag = 0;
   itm = NULL;
 
   itm = get_spead_item_with_id(ig, SPEAD_TS_ID);
   if (itm == NULL){
-    fprintf(stderr, "%s: cannot find item with id 0x%x\n", __func__, SPEAD_TS_ID);
+    fprintf(stderr, "%s: No timestamp data found (id: 0x%x)\n", __func__, SPEAD_TS_ID);
     return -1;
   }
    // TS is 5 byte unsigned 
   ts = (int)itm->i_data[0] + (int)itm->i_data[1] * 256 + (int)itm->i_data[2] * 256 * 256 + (int)itm->i_data[3] * 256 * 256 * 256 + (int)itm->i_data[4] * 256 * 256 * 256 * 256;
  
-  itm = get_spead_item_with_id(ig, SPEAD_BF_DATA_ID);
+  itm = get_spead_item_with_id(ig, SPEAD_BF_DATA_ID_0);
   if (itm == NULL){
-    fprintf(stderr, "%s: cannot find item with id 0x%x\n", __func__, SPEAD_BF_DATA_ID); 
+   itm = get_spead_item_with_id(ig, SPEAD_BF_DATA_ID_1);
+   if (itm == NULL){
+    fprintf(stderr, "%s: No beamformer payload data found (BF0 / BF1).\n", __func__); 
     return -1;
+   }
   }
 
   // check that the heap size matches our expectations
@@ -209,7 +226,6 @@ int spead_api_callback(struct spead_api_module_shared *s, struct spead_item_grou
    fprintf(stderr,"%s: Expecting heap size of %i, got %i\n",__func__,EXPECTED_HEAP_LEN, itm->i_data_len);
    return -1;
   }
-
 
   ss = get_data_spead_api_module_shared(s);
 
@@ -230,13 +246,10 @@ int spead_api_callback(struct spead_api_module_shared *s, struct spead_item_grou
   if (ss->prior_ts == 0) {
    if (ss->master_pid == 0) ss->master_pid = getpid();
     // make sure we know who is boss
-   if (ss->flag[3] == 1) { snprintf(ss->filename,100,"/data1/latest/%llu.dat",ts); ss->flag[3]=1; }
-   else { snprintf(ss->filename,100,"/data2/latest/%llu.dat",ts); ss->flag[3]=1;}
+
+   snprintf(ss->filename,255,"%s/%llu.dat",ss->prefix, ts); 
    fprintf(stderr,"%s: Requesting new file %s\n",__func__,ss->filename);
 
-   //w_fd2 = open(ss->filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-   //write(w_fd2,"\x93NUMPY\x01\x00V\x00{'descr': '|i1', 'fortran_order': False, 'shape': (128000, 1024, 2), }               \n",96);
-   //fprintf(stderr,"Master file open %s\n",ss->filename);
    ss->prior_ts = ts;
    fprintf(stderr,"Reset prior_ts base to %llu. Master pid set to %d\n",ts,ss->master_pid);
    set_data_spead_api_module_shared(s, ss, sizeof(struct snap_shot));
@@ -259,18 +272,9 @@ int spead_api_callback(struct spead_api_module_shared *s, struct spead_item_grou
    prior_ts = ss->prior_ts;
   }
 
-
-  /*if (w_fd2 == NULL) { 
-   w_fd2 = open(ss->filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-   fprintf(stderr,"Opening child file %s with prior_ts %llu\n", ss->filename, ss->prior_ts);
-  } */ 
-
-  //fstat(w_fd2, &sb);
-  //fprintf(stderr,"\n\nLast file modification %s in thread %d with prior_ts %i\n\n\n", ctime(&sb.st_mtime), getpid(), prior_ts);
-  
   heap_count++;
 
-  if (write_bf_data(itm->i_data,itm->i_data_len, (ts - ss->prior_ts) / 4) < 0) {
+  if (write_bf_data(itm->i_data, itm->i_data_len, (ts - ss->prior_ts) / 4, ss->fullband ? 1 : 2, ss->transpose) < 0) {
     fprintf(stderr,"%s: failed to write beamformer data to disk\n",__func__);
     return -1;
   }
